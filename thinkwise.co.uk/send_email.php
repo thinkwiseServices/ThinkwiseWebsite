f3781f3d152031c81435440f63ed8743e9253202
thinkwiseServices <contact@thikwiseservices.co.uk>
2024-03-21 13:50:06 +0000

    Added email

---
+++ thinkwise.co.uk/send_email.php
@@ -0,0 +1,20 @@
+<?php
+// Collect form data
+$name = $_POST["fullName"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$subject = $_POST["subject"];
+
+// Recipient
+$to = $_POST["niravgoyani007@gmail.com"];
+
+// Compose email
+$body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+// Send email (using PHP's mail function)
+if (mail($to, $subject, $body)) {
+    echo "Email sent successfully!"; // Success message back to the user
+} else {
+    echo "Error sending email.";  // Error message
+}
+?>
