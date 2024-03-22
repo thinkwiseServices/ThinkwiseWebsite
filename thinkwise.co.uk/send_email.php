4372f5320959065522abb94ed0af31d195471afe
thinkwiseServices <contact@thikwiseservices.co.uk>
2024-03-22 13:24:40 +0000

    Added email

--- thinkwise.co.uk/send_email.php
+++ thinkwise.co.uk/send_email.php
@@ -1,20 +1,47 @@
 <?php
-// Collect form data
-$name = $_POST["fullName"];
-$email = $_POST["email"];
-$message = $_POST["message"];
-$subject = $_POST["subject"];
 
-// Recipient
-$to = $_POST["niravgoyani007@gmail.com"];
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-// Compose email
-$body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+$name = $_POST['fullName'];
+$email = $_POST['email'];
+$msg = $_POST['message'];
 
-// Send email (using PHP's mail function)
-if (mail($to, $subject, $body)) {
-    echo "Email sent successfully!"; // Success message back to the user
-} else {
-    echo "Error sending email.";  // Error message
+//Load Composer's autoloader
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+require 'PHPMailer/Exception.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+//Server settings
+$mail->isSMTP();                                            //Send using SMTP
+$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = 'niravgoyani007@gmail.com';                     //SMTP username
+$mail->Password   = 'ltqosckteyhrtxlc';                               //SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+//Recipients
+$mail->setFrom('niravgoyani007@gmail.com', 'Mailer');
+$mail->addAddress('nirgoyani70@gmail.com', 'Joe User');     //Add a recipient
+
+//Content
+$mail->isHTML(true);                                  //Set email format to HTML
+$mail->Subject = 'Here is the subject';
+$mail->Body    = "Sender Name - $name <br> Sender Email - $email <br> message - $msg" ;
+
+$mail->send();
+echo 'Message has been sent';
+} catch (Exception $e) {
+echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+
 ?>
