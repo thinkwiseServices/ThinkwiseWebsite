336c2d21ff5a500c92cff1514c2b3f6042a4dbd3
thinkwiseServices <contact@thikwiseservices.co.uk>
2024-04-03 13:36:32 +0100

    Added email

--- thinkwise.co.uk/send_email.php
+++ thinkwise.co.uk/send_email.php
@@ -21,8 +21,8 @@ if (isset($_POST["submit"])) {
             $mail->isSMTP();                           // Set mailer to use SMTP
             $mail->Host = 'smtp.office365.com';          // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                    // Enable SMTP authentication
-            $mail->Username = 'nirav.goyani@thinkwiseservices.co.uk'; // SMTP username
-            $mail->Password = 'Inspiron.15';         // SMTP password
+            $mail->Username = 'contact@thinkwiseservices.co.uk'; // SMTP username
+            $mail->Password = '';         // SMTP password
             $mail->SMTPSecure = 'tls';                 // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                         // TCP port to connect to
 
